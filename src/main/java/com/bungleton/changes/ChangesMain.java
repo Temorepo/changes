5e7f3e229a94547e707e6a4ec844bec59714eeb4
Charlie Groves <charlie.groves@gmail.com>
2011-02-12 19:49:28 -0800

    Dump all the fields in classes in dependent jars using asm

--- src/main/java/com/bungleton/changes/ChangesMain.java
+++ src/main/java/com/bungleton/changes/ChangesMain.java
@@ -1,14 +1,42 @@
 package com.bungleton.changes;
 
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.objectweb.asm.ClassAdapter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.commons.EmptyVisitor;
 import org.sonatype.aether.artifact.Artifact;
 
 public class ChangesMain {
 	public static void main(String[] args) throws Exception
 	{
-        DependencyResolver resolver = new DependencyResolver();
+		for (Artifact afact : new DependencyResolver().resolve("com.samskivert:samskivert:1.2")) {
+			JarFile jar = new JarFile(afact.getFile(), false);
+			Enumeration<JarEntry> entries = jar.entries();
+			while (entries.hasMoreElements()) {
+				JarEntry entry = entries.nextElement();
+				if (!entry.getName().endsWith(".class")) {
+					continue;
+				}
 
-		for (Artifact artifact : resolver.resolve("com.samskivert:samskivert:1.2")) {
-			System.out.println(artifact.getFile());
+				ClassReader reader = new ClassReader(jar.getInputStream(entry));
+				reader.accept(new ClassAdapter(new EmptyVisitor()) {
+					@Override public void visit(int version, int access, String name,
+							String signature, String superName, String[] interfaces) {
+						System.out.println(name);
+
+					}
+
+					@Override public FieldVisitor visitField(int access, String name, String desc,
+							String signature, Object value) {
+						System.out.println("  " + name);
+						return null;
+					}
+				}, 0);
+			}
 		}
 	}
 }
